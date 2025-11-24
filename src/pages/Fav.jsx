2e6481a2eaa28e48c5c338388eedd4387f8cb310
Abi-390 <dekaabinash07@gmail.com>
2025-11-25 02:44:58 +0530

    spell mistake corrected

--- src/pages/Fav.jsx
+++ src/pages/Fav.jsx
@@ -6,7 +6,7 @@ const Fav = () => {
 const favourite = JSON.parse(localStorage.getItem("fav"))||[];
 
 const renderrecipes = favourite.map((recipe) => (<RecipeCard recipe={recipe} key={recipe.id}/>))
-  return <div className="flex flex-wrap">{favourite.length>0?renderrecipes:"No favourites found!"}</div>
+  return <div className="flex flex-wrap">{favourite.length>0?renderrecipes:"No favorites found!"}</div>
    
   
 };
